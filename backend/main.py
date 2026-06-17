4c50f815386b7d79e278c2c3cda77392e9fb1bd5
InfoTCube <infocube.yout@gmail.com>
2026-06-17 18:19:44 +0200

    feat: implement lobby service

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 
 from backend.api.routes.auth import router as auth_router
+from backend.api.routes.lobbies import router as lobbies_router
+from backend.api.routes.matches import router as matches_router
 from backend.api.routes.puzzles import router as puzzles_router
 from backend.api.routes.ws import router as ws_router
-from backend.api.routes.matches import router as matches_router
 
 app = FastAPI(title="Swordoku")
 
 app.include_router(auth_router)
+app.include_router(lobbies_router)
+app.include_router(matches_router)
 app.include_router(puzzles_router)
 app.include_router(ws_router)
-app.include_router(matches_router)
 
 
 @app.get("/health")
